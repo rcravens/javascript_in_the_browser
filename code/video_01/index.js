14e527a8fa72d668a90c0de25c51678a657337d7
rcravens <bob.cravens@gmail.com>
2025-08-24 11:30:55 -0500

    wip

--- code/video_01/index.js
+++ code/video_01/index.js
@@ -1,7 +1,7 @@
 // TODO: JS goes here!
 console.clear();
 console.log('Module: The Browser and the DOM');
-console.log('Video: Selecting and Modifying Elements');
+console.log('Video: Browser Runtime Environment');
 
 /*
 	•	JS Foundations: https://tekcasts.com/series/javascript-for-beginners
@@ -10,6 +10,52 @@ console.log('Video: Selecting and Modifying Elements');
 	•	What the DOM is (tree structure of nodes)
 	•	Relationship between HTML, CSS, JS, and the DOM
 	•	Document object (document) as entry point
-	•	DOM vs BOM (Browser Object Model) distinction
+	•	DOM (document) vs BOM (Browser Object Model) (window) distinction
 	•	Inspecting the DOM with browser dev tools
  */
+
+// --------------- DOM - Document Object Model (Data + API)
+
+console.log('DOM', document);
+
+// Selecting Document Elements
+const imgs = document.querySelectorAll('img');
+console.log(imgs);
+
+// View element properties / attributes
+console.log(imgs[0].style);
+
+// Modify element properties / attributes
+imgs[0].style.border = '10px solid red';
+
+// Modify elements based on events
+imgs[1].addEventListener('mouseenter', () => {
+    console.log('Mouse Enter');
+    imgs[1].style.border = '10px solid red';
+});
+imgs[1].addEventListener('mouseleave', () => {
+    console.log('Mouse Leave');
+    imgs[1].style.border = 'none';
+});
+
+// --------------- BOM - Browser Object Model (Data + API)
+// https://www.w3schools.com/js/js_window.asp
+
+console.log('BOM', window);
+
+// Examples
+window.console.log('Our familiar friend belongs here.'); // Generally don't need to use 'window.' to resolve these functions
+
+window.alert('This is a message');
+alert('This is another message');
+
+function alert(msg) {
+    window.alert('PREFIX: ' + msg);
+}
+
+console.log(localStorage);
+
+console.log(navigator.geolocation);
+
+console.log(fetch);
+
