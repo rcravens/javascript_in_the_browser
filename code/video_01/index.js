0b30361ce46c77d2d3a6452b7280bd1556c5d7a5
rcravens <bob.cravens@gmail.com>
2025-08-23 19:29:09 -0500

    wip

--- code/video_01/index.js
+++ code/video_01/index.js
@@ -5,6 +5,8 @@ console.log('Video: Selecting and Modifying Elements');
 
 /*
 	•	JS Foundations: https://tekcasts.com/series/javascript-for-beginners
+	•	JS Code, JS Engine, and JS Runtime
+	•	JS Engines for Major Browsers (John Dakshak): https://hackmd.io/@JohnD/HyLe9j_hyg
 	•	What the DOM is (tree structure of nodes)
 	•	Relationship between HTML, CSS, JS, and the DOM
 	•	Document object (document) as entry point
