5d1c11c0da2f1899dfbce79c39fe9701e26386d8
rcravens <bob.cravens@gmail.com>
2025-08-29 11:59:45 -0500

    wip

--- code/video_16/index.js
+++ code/video_16/index.js
@@ -4,19 +4,18 @@ console.log('Module: Frameworks and Libraries');
 console.log('Video: Framework & Library Overview');
 
 /*
-	•	What Alpine.js is (reactivity, HTML-driven)
-	•	Directives (x-data, x-bind, x-on, x-show)
-	•	Simple reactive state example
-	•	Comparison to Vue/React in philosophy (but lighter)
-	•	Example: Alpine-powered interactive component (Stock Picker App)
+	•	Libraries & Frameworks
  */
 
 //  charts - chart.js, highcharts, d3js, apexcharts
+
 //  maps - google maps api, open streetmap, leaflet
-//  dom manipulation - react, vue.js, angular, backbone.js, jquery
+
+//  dom reactivity - alpine.js, react, vue.js, angular, backbone.js, jquery
+
 //  forms - parsley, formvalidation, validate.js, react
-//  animations - animejs, typed.js
 
-//  frameworks - alpine.js, react, vue.js, angular, ember.js, svelte, backbone.js
+//  animations - aos, animejs, typed.js
 
 
+//  frameworks - react, next.js, vue.js, angular, ember.js, svelte, backbone.js
