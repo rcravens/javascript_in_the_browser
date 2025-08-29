bd0a0c5f9423ac68e6084631453fe2e8c15badea
rcravens <bob.cravens@gmail.com>
2025-08-29 09:36:26 -0500

    wip

--- code/video_14/index.js
+++ code/video_14/index.js
@@ -1,4 +1,5 @@
 // TODO: JS goes here!
+
 console.clear();
 console.log('Module: Structuring Applications');
 console.log('Video: Building a Small Project');
@@ -6,8 +7,8 @@ console.log('Video: Building a Small Project');
 /*
 	Stock Picker Application
 
-    1. Stock Data Model
-	2. Stock API (external data source + mocked data)
+	1. Stock API (external data source + mocked data)
+    2. Stock Data Model
 	3. Stock Fetcher (input + button)
 	4. Stock Table (table + rows + remove)
 	5. Persist Stocks
